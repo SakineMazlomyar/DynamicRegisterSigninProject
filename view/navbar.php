7bfb53420cb4b538b8ff94a22b74023ecc05df5d
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-14 10:44:58 +0100

    addedNavbar

---
+++ view/navbar.php
@@ -0,0 +1,11 @@
+<?php
+    return "
+    <nav>
+        <ul>
+            <li><a href='index.php?page=register'>Get Register page</a></li>
+            <li><a href='index.php?page=login'>Get Login  page</a></li>
+        </ul>
+    </nav>
+    
+    "
+?>
