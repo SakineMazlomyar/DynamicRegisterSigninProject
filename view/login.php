3a0cc107c6bc418eb23d3588deb7e1b20d6e2815
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-14 10:45:08 +0100

    addedLogin

---
+++ view/login.php
@@ -0,0 +1,11 @@
+<?php 
+    return "
+    <form>
+        <label>Username</label>
+        <input type='text' username='username'>
+        <input type='submit' value='login' name='login'>    
+    </form>
+    
+    "
+
+?>
