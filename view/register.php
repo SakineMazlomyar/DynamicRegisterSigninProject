fd75ac957b8ebd0403c946aa69673661ba940fb2
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-14 11:44:49 +0100

    modifiedForm

--- view/register.php
+++ view/register.php
@@ -1,7 +1,10 @@
 <?php 
     return "
-    <form>
-        <input type='text' name='name'>
+    <form method='post' action='./view/register-code.php'>
+        <label>Name</label><br>
+        <input type='text' name='name'><br>
+        <label>Password</label><br>
+        <input type='text' name='password'><br>
         <input type='submit' value='save' name='new-name'>    
     </form>
     
